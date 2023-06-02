67f4f1f743693f7b21bd993ab40eb9f9ef0b1133
Winston Weinert <git@winny.tech>
2023-06-02 03:08:04 -0500

    feat: title

--- src/conf.lua
+++ src/conf.lua
@@ -1,11 +1,15 @@
-love.conf = function(t)
+function love.conf(t)
   -- Based on https://gitlab.com/technomancy/bussard/-/blob/master/conf.lua
   for index, a in pairs(arg) do
     if a == "--headless" then
       t.window, t.modules.window, t.modules.graphics = false, false, false
       table.remove(arg, index)
-      break -- This code isn't safe to run the same loop body twice because of
-            -- the table.remove.
+      -- This code isn't safe to run the same loop body twice because of the
+      -- table.remove.
+      break
     end
+  end
+  if t.window then
+    t.window.title = "Super Rogue"
   end
 end
