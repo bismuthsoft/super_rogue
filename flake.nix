e62e15070d6435cc00332d146cfadd5a18059084
Sam Pagenkopf <ssaammp@gmail.com>
2023-06-16 23:30:12 -0500

    upgrade nix

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "A very basic flake";
 
-  inputs.nixpkgs.url = "nixpkgs/nixos-22.11";
+  inputs.nixpkgs.url = "nixpkgs/nixos-23.05";
 
   outputs = { self, nixpkgs }: let
     pkgs = nixpkgs.legacyPackages.x86_64-linux.buildPackages;
