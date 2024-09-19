9c4ba5428e2f3966aa5376284cf0dfbb876161d4
Winston (Winny) Weinert <git@winny.tech>
2024-09-19 16:15:12 -0500

    fix: potential typo/scope fixes (#77)
    * fix: potential typo/scope fixes
    
    * fix log message for killer tomato becoming tomato
    
    * fix: fennel aware tracebacks
    
    See https://sr.ht/~benthor/absolutely-minimal-love2d-fennel/#fennel-aware-stack-traces
    
    * DRY-ify fennel loader
    
    ---------
    
    Co-authored-by: Sam Pagenkopf <ssaammp@gmail.com>

--- src/main.lua
+++ src/main.lua
@@ -3,24 +3,20 @@
 local fennel = require("lib.fennel")
 local make_love_searcher = function(env)
   return function(module_name)
-    local path = module_name:gsub("%.", "/") .. ".fnl"
-    if love.filesystem.getInfo(path) then
-      return function(...)
-        local code = love.filesystem.read(path)
-        return fennel.eval(code, {env=env}, ...)
-      end, path
-    end
-    path = module_name:gsub("%.", "/") .. "/init.fnl"
-    if love.filesystem.getInfo(path) then
-      return function(...)
-        local code = love.filesystem.read(path)
-        return fennel.eval(code, {env=env}, ...)
-      end, path
+    for _, filename in ipairs({".fnl", "/init.fnl"}) do
+      local path = module_name:gsub("%.", "/") .. filename
+      if love.filesystem.getInfo(path) then
+        return function(...)
+          local code = love.filesystem.read(path)
+          return fennel.eval(code, {env=env, filename=path}, ...)
+        end
+      end
     end
   end
 end
 
 table.insert(package.loaders, make_love_searcher(_G))
 table.insert(fennel["macro-searchers"], make_love_searcher("_COMPILER"))
+debug.traceback = fennel.traceback
 
 require("game")
